04815163712b8bed44ebc06eb5678ec942954303
mattiadelmonaco <mattia.delmonaco@gmail.com>
2025-04-10 12:48:29 +0200

    feat: create new file for functions, include it in index.php

--- index.php
+++ index.php
@@ -1,30 +1,6 @@
 <?php
 
-$passwordLength = $_GET["password-length"];
-
-function passwordGenerator() {
-    global $passwordLength;
-    $password = "";
-
-    for($i = 1; $i <= $passwordLength; $i++) {
-        $randomChar = rand(1,5); // 1 = numero, 2 = minuscola, 3 = maiuscola, 4 = primo set di simboli, 5 = secondo set di simboli
-
-        if ($randomChar === 1) {
-            $char = rand(1,9);
-        } else if ($randomChar === 2) {
-            $char = chr(rand(97, 122));
-        } else if ($randomChar === 3) {
-            $char = chr(rand(65, 90));
-        } else if ($randomChar === 4) {
-            $char = chr(rand(33, 47));
-        } else if ($randomChar === 5) {
-            $char = chr(rand(58, 64));
-        }
-       $password .= $char;
-    }
-
-    return $password;
-}
+include_once "./functions.php"
 
 ?>
 
