e8545b388e4ce511fe03b6238f575b0bb27eeafc
mattiadelmonaco <mattia.delmonaco@gmail.com>
2025-04-10 12:45:33 +0200

    feat: add form to get password length, function to generate random password with letter, numbers and simbols and show it in page

--- index.php
+++ index.php
@@ -1,3 +1,34 @@
+<?php
+
+$passwordLength = $_GET["password-length"];
+
+function passwordGenerator() {
+    global $passwordLength;
+    $password = "";
+
+    for($i = 1; $i <= $passwordLength; $i++) {
+        $randomChar = rand(1,5); // 1 = numero, 2 = minuscola, 3 = maiuscola, 4 = primo set di simboli, 5 = secondo set di simboli
+
+        if ($randomChar === 1) {
+            $char = rand(1,9);
+        } else if ($randomChar === 2) {
+            $char = chr(rand(97, 122));
+        } else if ($randomChar === 3) {
+            $char = chr(rand(65, 90));
+        } else if ($randomChar === 4) {
+            $char = chr(rand(33, 47));
+        } else if ($randomChar === 5) {
+            $char = chr(rand(58, 64));
+        }
+       $password .= $char;
+    }
+
+    return $password;
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,7 +39,21 @@
 </head>
 
 <body>
+    <header>
+        <h1>Strong Password Generator</h1>
+        <h2>Genera una password sicura</h2>
+    </header>
+    <section>
+        <form action="" method="GET">
+            <label for="password-length">Lunghezza password</label>
+            <input id="password-length" name="password-length" type="number" min="5" max="15" required>
 
+            <button type="submit">Genera</button>
+
+        </form>
+    </section>
+
+    <h1>La tua password sicura è: <?php echo passwordGenerator() ?></h1>
 </body>
 
 </html>
